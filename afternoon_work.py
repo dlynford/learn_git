a5c86ac251601913fa3fdfb65866924c9cbb6729
andrewlynford <dlynford@hotmail.com>
2022-06-01 00:53:31 -0400

    Wrote a new module.

---
+++ afternoon_work.py
@@ -0,0 +1,13 @@
+# 6/1/2022
+
+# I am learning how to commit, push, and pull files on Github.
+
+def main():
+    digits = []
+    for i in range(25):
+        digits.append(i)
+    return digits
+
+
+if __name__ == '__main__':
+    print(main(), 'hello')
